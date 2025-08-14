e61685718a2934b26d52a6532ed32c903d25f899
singlanehal <136306866+singlanehal@users.noreply.github.com>
2025-08-14 14:25:04 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string res = "";
+        for(int i=0;i+2<num.size();i++)
+        {
+            if(num[i]==num[i+1] && num[i]==num[i+2])
+            {
+                res = max(res,num.substr(i,3));
+            }
+        }
+        return res;
+    }
+};
